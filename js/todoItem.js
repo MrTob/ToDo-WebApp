a50a7bed87275091c6335a5d5c2f186c82c3e37e
MrTob <mr.tobtv@gmail.com>
2020-11-12 23:09:34 +0100

    ✨feature: added classes

---
+++ js/todoItem.js
@@ -0,0 +1,22 @@
+export default class ToDoItem{
+    constructor(){
+        this.id = null;
+        this.item = null;
+    }
+
+    getId(){
+        return this.id
+    }
+
+    setId(id){
+        this.id = id;
+    }
+
+    getItem(){
+        return this.item;
+    }
+
+    setItem(item){
+        this.item = item;
+    }
+}
